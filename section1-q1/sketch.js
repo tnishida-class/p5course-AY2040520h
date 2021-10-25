94fe1bdc1c93b2ab8b1f5897cdd0220666da9a74
AY2040520h <92346101+AY2040520h@users.noreply.github.com>
2021-10-26 00:33:26 +0900

    section2-q1 change

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -7,8 +7,8 @@ function setup(){
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
-  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  arc(100 - 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
+  arc(100 + 26, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  arc(100 - 26, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
   // BLANK[1]
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
@@ -17,5 +17,14 @@ function setup(){
   const green = color(22, 131, 46);
   const gray = color(77);
 
-
+  fill(14, 47, 146);
+  noStroke();
+  ellipse(300, 100, 95, 95);
+  fill(255);
+  ellipse(293, 93, 90, 90);
+  fill(22, 131, 46);
+  ellipse(291, 91, 85, 85);
+  fill(255);
+  ellipse(289, 97, 80, 80);
+  
 }
--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  createCanvas(100,100);
+  for(let i = 0 - 1; i < 10; i++){
     // BLANK[1]
-  }
+    noFill()
+    if(i >= 0 && i <= 4){
+      stroke(0, 0, 255);
+    }
+    else{
+      stroke(255, 0, 0);
+    }
+    ellipse(50, 50, i*10 + 10);}
 }
